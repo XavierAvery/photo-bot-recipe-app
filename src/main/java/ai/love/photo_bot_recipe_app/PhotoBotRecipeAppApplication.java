5f3093b95375b1659cbeaf867e8836105836aa93
XavierAvery <xavery00@gmail.com>
2025-03-16 15:32:54 -0400

    Initial Project setup for AI app

---
+++ src/main/java/ai/love/photo_bot_recipe_app/PhotoBotRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package ai.love.photo_bot_recipe_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoBotRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoBotRecipeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/love/photo_bot_recipe_app/PhotoBotRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.love.photo_bot_recipe_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoBotRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
